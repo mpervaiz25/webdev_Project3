4db3998e103a547a25d87fec6dba2929d3986e1d
mpervaiz25 <mpervaiz@hawk.iit.edu>
2024-12-07 20:36:56 -0600

    adding a window marker

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ function initMap() {
     zoom: 14,
     center: iitLocation,
   });
+	 // Add custom marker
+  const marker = new google.maps.Marker({
+    position: iitLocation,
+    map: map,
+    title: "Illinois Tech",
+  });
+	// Info window on marker
+  const infoWindow = new google.maps.InfoWindow({
+    content: "<h3>Illinois Institute of Technology</h3><p>A Level 1 Arboretum Campus!</p>",
+  });
+  marker.addListener("click", () => {
+    infoWindow.open(map, marker);
+  });
